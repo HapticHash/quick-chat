5b6351238af6028b79de6f0872280b0c223a4e8e
Harshit S <harshitks2203@gmail.com>
2023-07-18 21:25:26 -0400

    auth setup

---
+++ app/(site)/page.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 bg-gray-100">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image
+          src="/images/logo.png"
+          alt="Logo"
+          height={100}
+          width={200}
+          className="mx-auto w-auto"
+        />
+        <h2 className="mt-8 text-center text-3xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
+      </div>
+      {/* AuthForm */}
+    </div>
+  );
+}
