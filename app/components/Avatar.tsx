087ecbea2f304b5699e6317c0b1a565bd15b12ed
Harshit S <harshitks2203@gmail.com>
2023-07-24 21:18:40 -0400

    added profile drawer

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,10 +1,6 @@
 "use client";
 
 import { User } from "@prisma/client";
-import { useMemo } from "react";
-import { createAvatar } from "@dicebear/core";
-import * as style from "@dicebear/fun-emoji";
-
 import Image from "next/image";
 
 interface AvatarProps {
@@ -12,46 +8,6 @@ interface AvatarProps {
 }
 
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
-  // const genAvatar = useMemo(() => {
-  //   return createAvatar(style, {
-  //     size: 24,
-  //     scale: 50,
-  //     radius: 0,
-  //     backgroundType: ["gradientLinear", "solid"],
-  //     backgroundColor: [
-  //       "059ff2",
-  //       "71cf62",
-  //       "d84be5",
-  //       "d9915b",
-  //       "f6d594",
-  //       "fcbc34",
-  //     ],
-  //     clip: true,
-  //     randomizeIds: true,
-  //     eyes: [
-  //       "closed",
-  //       "closed2",
-  //       "cute",
-  //       "glasses",
-  //       "love",
-  //       "shades",
-  //       "stars",
-  //       "wink",
-  //       "wink2",
-  //     ],
-  //     mouth: [
-  //       "cute",
-  //       "faceMask",
-  //       "kissHeart",
-  //       "lilSmile",
-  //       "shy",
-  //       "smileTeeth",
-  //       "tongueOut",
-  //       "wideSmile",
-  //     ],
-  //   }).toDataUriSync();
-  // }, []);
-
   return (
     <div className="relative">
       <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11 drop-shadow-md">
--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -5,6 +5,7 @@ import useRoutes from "@/app/hooks/useRoutes";
 import { useState } from "react";
 import { User } from "@prisma/client";
 import Avatar from "../Avatar";
+import SettingsModal from "./SettingsModal";
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -18,6 +19,11 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
 
   return (
     <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
       <div className=" hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-y-auto  lg:bg-white lg:border-r-[1px] lg:pb-4 lg:flex lg:flex-col justify-between">
         <nav className="mt-4 flex flex-col justify-between">
           <ul role="list" className="flex flex-col items-center space-y-2">
